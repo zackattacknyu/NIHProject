7d256aa1474622eb59f93d4663c414f6188e7fc4
zackattacknyu <zackattacknyu@gmail.com>
2015-06-25 18:03:48 -0400

    final reg script before running test for the night

--- matlab/testRegistration.m
+++ matlab/testRegistration.m
@@ -5,19 +5,22 @@
 %folderNames = {'ser_5005','ser_5007'};
 parentDir = 'timeSeries1_5005-5007/';
 fixedFolderName = strcat(parentDir,'ser_5005/');
-movingFolderName = strcat(parentDir,'ser_5007/');
+movingFolderName = strcat(parentDir,'ser_5006/');
+movingFolderName2 = strcat(parentDir,'ser_5007/');
 thresholdFactor = 0.85;
 
 dcmDataFixed = single(getDCMFolderDataThreshold(fixedFolderName,thresholdFactor));
 dcmDataMoving = single(getDCMFolderDataThreshold(movingFolderName,thresholdFactor));
+dcmDataMoving2 = single(getDCMFolderDataThreshold(movingFolderName2,thresholdFactor));
 
 [optimizer, metric]  = imregconfig('monomodal');
 [dcmDataRegistered,regInfo,regT] = ...
     imregister2(dcmDataMoving,dcmDataFixed,'affine',optimizer,metric,'DisplayOptimization',true); 
-
+[dcmDataRegistered2,regInfo2,regT2] = ...
+    imregister2(dcmDataMoving2,dcmDataFixed,'affine',optimizer,metric,'DisplayOptimization',true); 
 %%
 %shows the newly registered images
-imtool3D(dcmDataFixed+dcmDataRegistered);
+%imtool3D(dcmDataFixed+dcmDataRegistered);
 
 %shows unregistered images
-imtool3D(dcmDataFixed+dcmDataMoving);
+%imtool3D(dcmDataFixed+dcmDataMoving);
