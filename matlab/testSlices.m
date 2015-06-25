0b9545f5acd9e862f541c35fc1c65bd572bcb262
zackattacknyu <zackattacknyu@gmail.com>
2015-06-25 10:14:11 -0400

    modularized the wronskian code

--- matlab/testSlices.m
+++ matlab/testSlices.m
@@ -16,24 +16,7 @@ numFrames = size(imagesFrames,2);
 frame1 = imagesFrames{sliceNum,1};
 frame2 = imagesFrames{sliceNum,2};
 
-%sets up the wronskian frame
-numRowsWFrame = size(frame1,1)-2*sR;
-numColWFrame = size(frame1,2)-2*sR;
-WFrame = zeros(numRowsWFrame,numColWFrame);
-
-%makes the wronskian frame
-sN = (sR*2+1)^2; %support N, number of pixels in support region
-for i = (1+sR):(numRowsWFrame-sR)
-   for j = (1+sR):(numColWFrame-sR)
-      regionX = frame1((i-sR):(i+sR),(j-sR):(j+sR));
-      regionY = frame2((i-sR):(i+sR),(j-sR):(j+sR));
-      regionX = regionX(:); regionY = regionY(:);
-      squaredSum = sum((regionX.^2)./(regionY.^2));
-      normalSum = sum(regionX./regionY);
-      wValue = (squaredSum+normalSum)/sN;
-      WFrame(i,j) = wValue;
-   end
-end
+WFrame = wronskian(frame1,frame2,sR);
 
 diffFrame = frame2-frame1;
 
---
+++ matlab/wronskian.m
@@ -0,0 +1,28 @@
+function [ WFrame ] = wronskian( frame1,frame2, sR )
+%WRONSKIAN Computes the wronskian change detector on two frames
+%   frame1 - baseline frame
+%   frame2 - current frame
+%   sR - support radius
+
+%sets up the wronskian frame
+numRowsWFrame = size(frame1,1)-2*sR;
+numColWFrame = size(frame1,2)-2*sR;
+WFrame = zeros(numRowsWFrame,numColWFrame);
+
+%makes the wronskian frame
+sN = (sR*2+1)^2; %support N, number of pixels in support region
+for i = (1+sR):(numRowsWFrame-sR)
+   for j = (1+sR):(numColWFrame-sR)
+      regionX = frame1((i-sR):(i+sR),(j-sR):(j+sR));
+      regionY = frame2((i-sR):(i+sR),(j-sR):(j+sR));
+      regionX = regionX(:); regionY = regionY(:);
+      squaredSum = sum((regionX.^2)./(regionY.^2));
+      normalSum = sum(regionX./regionY);
+      wValue = (squaredSum+normalSum)/sN;
+      WFrame(i,j) = wValue;
+   end
+end
+
+
+end
+
