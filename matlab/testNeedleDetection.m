c4f912d88e74822ad3a1aa104f8a57a389b9bdf7
zackattacknyu <zackattacknyu@gmail.com>
2015-07-14 17:50:08 -0400

    tried needle detection more

--- matlab/testNeedleDetection.m
+++ matlab/testNeedleDetection.m
@@ -13,16 +13,21 @@ coeff = pca([cInd rInd zInd]);
 
 needlecomp = coeff(:,1);
 %%
-xx = -300:300;
+
+%data is for Patient 8 atm
+xx = 0:300;
 xx = xx';
 xx = repmat(xx,[1 3]);
 yy = needlecomp';
-yy = repmat(yy,[601 1]);
+yy = repmat(yy,[301 1]);
 res = yy.*xx;
-%%
+
+offset = [155 109 207];
+offset = repmat(offset,[301 1]);
+res2 = res+offset;
 
 figure
 plot3(cInd,rInd,zInd,'r.');
 hold on
-plot3(res(:,1),res(:,2),res(:,3),'bx')
+plot3(res2(:,1),res2(:,2),res2(:,3),'bx')
 hold off
