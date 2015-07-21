8b31f57b96c8e006ec39f0ed8523dc81f42e9f4a
zackattacknyu <zackattacknyu@gmail.com>
2015-07-21 18:34:01 -0400

    basic needle detection working

--- matlab/testNeedleDetection.m
+++ matlab/testNeedleDetection.m
@@ -1,33 +1,51 @@
-threshold = 2900; %HU for needle
+%{
+
+This program works by thresholding the CT scan in order to only have points
+    with high HU value as those will have the needle.
+
+The threshold is the HU value used for this.
+
+It then finds the connected components and considers the components with a
+    sufficient number (defined by points threshold) of points to be the needles. 
+
+%}
+
+threshold = 2500; %HU for needle
+pointsThreshold = 100; %number of points before connected component is considered important
+
 fixedImgNeedle = single(fixedImg>threshold);
 
-needlePts = find(fixedImg>threshold);
-%%
-[rInd,cInd,zInd] = ind2sub(size(fixedImg),needlePts);
+components = bwconncomp(fixedImgNeedle);
+compPixelLists = components.PixelIdxList;
+numComps = length(compPixelLists);
+sizeComps = zeros(1,numComps);
+for ind = 1:numComps
+    sizeComps(ind) = length(compPixelLists{ind});
+end
+[numInComp,compIndices] = sort(sizeComps,'descend');
+numImpComps = find(numInComp<pointsThreshold, 1 )-1;
 
-%%
-
-coeff = pca([cInd rInd zInd]);
-
-%%
-
-needlecomp = coeff(:,1);
-%%
-
-%data is for Patient 8 atm
-xx = 0:300;
-xx = xx';
-xx = repmat(xx,[1 3]);
-yy = needlecomp';
-yy = repmat(yy,[301 1]);
-res = yy.*xx;
-
-offset = [155 109 207];
-offset = repmat(offset,[301 1]);
-res2 = res+offset;
+outputComp = cell(1,numImpComps);
+for j = 1:numImpComps
+    outputComp{j} = compPixelLists{compIndices(j)};
+end
 
 figure
-plot3(cInd,rInd,zInd,'r.');
 hold on
-plot3(res2(:,1),res2(:,2),res2(:,3),'bx')
+for compNum = 1:numImpComps
+    
+    [rInd,cInd,zInd] = ind2sub(size(fixedImg),outputComp{compNum});
+    pts = [rInd,cInd,zInd];
+    [coeff,score,latent] = pca(pts);
+    needleScore = score(:,1);
+    [scores,pointInds] = sort(needleScore);
+    needleStart = pts(pointInds(1),:);
+    needleEnd = pts(pointInds(end),:);
+    needlePts = [needleStart;needleEnd];
+    
+    plot3(needlePts(:,1),needlePts(:,2),needlePts(:,3),'rx','MarkerSize',20,'LineWidth',2);
+    plot3(pts(:,1),pts(:,2),pts(:,3),'b.')
+    
+end
+
 hold off
