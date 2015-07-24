bcd083d0dc33f57d2a49eec2b51351d7f0d0e8f6
zackattacknyu <zackattacknyu@gmail.com>
2015-07-24 16:48:49 -0400

    wrote tic and toc

--- matlab/call_7_24.m
+++ matlab/call_7_24.m
@@ -3,13 +3,34 @@ movingROI = movingImg3(rInds,cInds,zInds);
 movingROI2 = movingImg4(rInds,cInds,zInds);
 
 fSize=4;
+
 Nsteps=10;
+tic
 diffFrameBlockRandWalk_10 = getSlidingWindowBlocksRandWalk( fixedROI,movingROI,fSize,Nsteps );
+time1=toc
+
+tic
 diffFrameBlockRandWalk2_10 = getSlidingWindowBlocksRandWalk( fixedROI,movingROI2,fSize,Nsteps );
+time2=toc
+
+
 
 Nsteps=50;
-diffFrameBlockRandWalk_50 = getSlidingWindowBlocksRandWalk( fixedROI,movingROI,fSize,Nsteps );
-diffFrameBlockRandWalk2_50 = getSlidingWindowBlocksRandWalk( fixedROI,movingROI2,fSize,Nsteps );
+tic
+diffFrameBlockRandWalk_50 = getSlidingWindowBlocksRandWalk( fixedROI,movingROI,fSize,Nsteps );\
+time3=toc
 
+tic
+diffFrameBlockRandWalk2_50 = getSlidingWindowBlocksRandWalk( fixedROI,movingROI2,fSize,Nsteps );
+time4=toc
+
+
+
+
+tic
 diffFrameBlock = getSlidingWindowBlocks(fixedROI,movingROI,fSize);
+time5=toc
+
+tic
 diffFrameBlock2 = getSlidingWindowBlocks(fixedROI,movingROI2,fSize);
+time6=toc
