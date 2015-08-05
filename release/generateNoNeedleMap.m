e0b4b977a9f586822354349477677f4fd9a74d65
zackattacknyu <zackattacknyu@gmail.com>
2015-08-05 17:42:04 -0400

    commenting done

--- release/generateNoNeedleMap.m
+++ release/generateNoNeedleMap.m
@@ -1,6 +1,16 @@
 function [ thermalMapNoNeedle ] = generateNoNeedleMap( currentBaseROI,currentThermalMap,fSize,HUthreshold )
-%GENERATENONEEDLEMAP Summary of this function goes here
-%   Detailed explanation goes here
+%GENERATENONEEDLEMAP takes in 3D volume. makes voxels zero that contain needle
+%   INPUT:
+%       currentBaseROI - baseline scan ROI volume. 
+%                         used to find voxels which contain needle
+%       currentThermalMap - input 3D volume. will remain intact, except
+%                           that needle voxels will equal zero
+%       fSize - half-width of window used when generating thermal map
+%       HUthreshold - if a voxel is above this HU value, it is considered a
+%                     needle voxel
+%
+%   OUTPUT
+%       thermalMapNoNeedle - currentThermalMap with zeros for needle voxels
 
 thermalMapVol = zeros(size(currentBaseROI));
 thermalMapVol(1+fSize:end-fSize,1+fSize:end-fSize,:) = currentThermalMap;
--- release/generateRegressionOutput.m
+++ release/generateRegressionOutput.m
@@ -1,6 +1,15 @@
 function [ coeff,xx,yy ] = generateRegressionOutput( diffVals,tempVals,degree )
-%GENERATEREGRESSIONOUTPUT Summary of this function goes here
-%   Detailed explanation goes here
+%GENERATEREGRESSIONOUTPUT performs regression on input values
+%
+%   INPUT:
+%       diffVals - x values in regression
+%       tempVals - y values in regression
+%       degree - polynomial degree
+%
+%   OUTPUT
+%       coeff - coefficients of regression curve found
+%       xx - x values in regression curve
+%       yy - y values in regression curve
 
 coeff = polyfit(diffVals,tempVals,degree);
 xx = linspace(min(diffVals),max(diffVals),20);
