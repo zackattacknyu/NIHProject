28a36ae7dc22c3e582ab26efd5ada5eecc440705
zackattacknyu <zackattacknyu@gmail.com>
2015-08-05 17:53:54 -0400

    more commenting

--- release/getAverageFilteredBlocks.m
+++ release/getAverageFilteredBlocks.m
@@ -1,6 +1,14 @@
 function [ fixedImgConv ] = getAverageFilteredBlocks( fixedImg,filterSize )
-%GETAVERAGEFILTEREDBLOCKS Summary of this function goes here
-%   Detailed explanation goes here
+%GETAVERAGEFILTEREDBLOCKS gets volume where each slice is convoluted with
+%                         averaging filter
+%
+%   INPUT:
+%       fixedImg - input volume
+%       filterSize - Each slice will be filtered by convolution using n by
+%                   n averaging filter. This value is used for n
+%
+%   OUTPUT
+%       fixedImgConv - output block of filtered slices
 
 filter = fspecial('average',filterSize);
 fixedImgConv = zeros(size(fixedImg));
--- release/getConvDiffROI.m
+++ release/getConvDiffROI.m
@@ -1,6 +1,14 @@
 function [ diffROIConv ] = getConvDiffROI( baseROI,comparisonROI,fSize )
-%GETCONVDIFFROI Summary of this function goes here
-%   Detailed explanation goes here
+%GETCONVDIFFROI Obtains convolutional difference block
+%
+%   INPUT:
+%       baseROI - input baseline volume
+%       comparisonROI - input comparison volume
+%       fSize - filter size
+%
+%   OUTPUT
+%       diffROIConv - the volume where each voxel is the difference between 
+%                   the filtered base and comparison volume voxels
 
 fixedImgConv = getAverageFilteredBlocks(baseROI,fSize);
 movingImgConv = getAverageFilteredBlocks(comparisonROI,fSize);
