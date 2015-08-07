908017da700c70077ff0934f68b81f0ccc90761c
zackattacknyu <zackattacknyu@gmail.com>
2015-08-07 17:16:43 -0400

    work done

--- release/generateMeanTempVsRadius.m
+++ release/generateMeanTempVsRadius.m
@@ -1,14 +1,21 @@
-%TO BE RUN AFTER GENERATEHEATMAP[METHOD].M
-%   GENERATES GRAPH OF MEAN TEMPERATURE VERSUS RADIUS
+%{
+This generates the mean temperature vs radius graph
 
-tempImage = currentThermalMap(:,:,23);
+You need to specify the slice number and then you manually
+    select the ablation center
+%}
+
+%MANUALLY INPUT SLICE NUMBER HERE
+sliceNum=23;
+
+tempImage = currentThermalMap(:,:,sliceNum);
 figure
 imagesc(tempImage);
 colorbar;
 
 [centerR, centerC] = ginput;
 
-centR = floor(centerR); centC = floor(centerC);
+centR = floor(centerR(1)); centC = floor(centerC(1));
 numR = size(tempImage,2);
 numC = size(tempImage,1);
 [rr, cc] = meshgrid(1:numR,1:numC);
@@ -28,68 +35,3 @@ figure
 plot(0:maxRadii,averageTemps);
 xlabel('Radius of Region centered at Ablation Zone (pixels)');
 ylabel('Average Temperature in Region (Celcius)');
-%%
-
-%averageTempsPlot = {averageTemps};
-%averageTempsPlot = {averageTemps averageTempsPlot};
-%averageTempsPlot2 = {averageTempsPlot{1} plot2{1}};
-%averageTempsPlot = {averageTempsPlot2{1} averageTempsPlot2{2} averageTemps};
-NN = length(averageTempsPlot);
-averageTempsPlotTemp = cell(1,NN+1);
-for ind = 1:NN
-    averageTempsPlotTemp{ind} = averageTempsPlot{ind};
-end
-averageTempsPlotTemp{NN+1}=averageTemps;
-averageTempsPlot = averageTempsPlotTemp;
-save('currentAvgTemps.mat','averageTempsPlot')
-
-%%
-
-
-%%
-
-xVals = (0:maxRadii).*0.9823; %mm for all patients, except 9
-xVals2 = (0:maxRadii).*0.6549; %mm for pt 9
-
-figure
-plot(xVals2,averageTempsPlot{2},'r-');
-hold on
-%plot(xVals,averageTempsPlot{1},'b-');
-plot(xVals,averageTempsPlot{3},'g-');
-plot(xVals,averageTempsPlot{4},'b-');
-plot(xVals,averageTempsPlot{5},'k-');
-%plot(xVals,averageTempsPlot{6},'m-');
-hold off
-xlabel('Radius of Region centered at Ablation Zone (mm)');
-ylabel('Average Temperature in Region (Celsius)');
-legend('Patient 9',...
-    'Patient 5, baseline scan: 5012',...
-    'Patient 5, baseline scan: 5021',...
-    'Patient 6');
-
-%%
-
-
-figure
-plot(xVals2,averageTempsPlot{1},'r-');
-hold on
-plot(xVals,averageTempsPlot{3},'b-');
-plot(xVals,averageTempsPlot{6},'g-');
-hold off
-xlabel('Radius of Region centered at Ablation Zone (mm)');
-ylabel('Average Temperature in Region (Celcius)');
-legend('Patient 9',...
-    'Patient 5, baseline 1',...
-    'Patient 3');
-
-figure
-plot(xVals2,averageTempsPlot{2},'r-');
-hold on
-plot(xVals,averageTempsPlot{4},'b-');
-plot(xVals,averageTempsPlot{5},'g-');
-hold off
-xlabel('Radius of Region centered at Ablation Zone (mm)');
-ylabel('Average Temperature in Region (Celcius)');
-legend('Patient 4',...
-    'Patient 5, baseline 3',...
-    'Patient 6');
