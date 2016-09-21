0603a0d109cf9dce4a4e99778058a473b44ff991
zackattacknyu <zackattacknyu@gmail.com>
2016-09-21 13:19:57 -0700

    df

--- release/getDCMFolderData_Old.m
+++ release/getDCMFolderData_Old.m
@@ -22,21 +22,21 @@ intercept = 0;
 for i = 1:numFiles
     filename = filesInFolder(i,1).name;
     filepath = strcat(dirName,filename);
-    %if(length(filename) < 4)
-    %   continue; %do not include it 
-    %end
-    %if(strcmp(filename(end-n+1:end), suffix))
-    dcmData{index} = dicomread(filepath); 
-    dcmInfo = dicominfo(filepath);
-    if(isfield(dcmInfo,'RescaleIntercept'))
-	   slope=dcmInfo.RescaleSlope;
-	   intercept=dcmInfo.RescaleIntercept;
-	   FamilyName = dcmInfo.PatientName.FamilyName;
-	   StudyDate = dcmInfo.StudyDate;
-	   SeriesNumber = dcmInfo.SeriesNumber;
+    if(length(filename) < 4)
+       continue; %do not include it 
     end
-    index = index + 1;
-    %end
+    if(strcmp(filename(end-n+1:end), suffix))
+       dcmData{index} = dicomread(filepath); 
+       dcmInfo = dicominfo(filepath);
+       if(isfield(dcmInfo,'RescaleIntercept'))
+           slope=dcmInfo.RescaleSlope;
+           intercept=dcmInfo.RescaleIntercept;
+           FamilyName = dcmInfo.PatientName.FamilyName;
+           StudyDate = dcmInfo.StudyDate;
+           SeriesNumber = dcmInfo.SeriesNumber;
+       end
+       index = index + 1;
+    end
 end
 dcmData = dcmData(1:(index-1));
 
